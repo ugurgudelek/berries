21ceef908594c84ba6e5bf2a0591e35642f2f394
Arda <arda@my.PC>
2017-04-07 11:25:45 +0300

    Reverting main.py changes for branch features

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import metrics as mt
-import classes as cls
 import yahoo_finance_io
 import datetime
 
@@ -48,6 +47,15 @@ def calculate_metrics(data, metric_functions):
     for func in metric_functions:
         ret.append(func(data))
 
+    # rsi_15_data = mt.rsi(data)
+    # sma_15_data = mt.sma(data)
+    # macd_15_5_data = mt.macd(data)
+    # macd_trigger_9_15_5 = mt.macd_trigger(data)
+    # willR = mt.williamsR(data)
+    # kdHist = mt.kdDiff(data)
+    # ultimateOs = mt.ulOs(data)
+    # mfIndex = mt.mfi(data)
+
     return np.asarray(ret)
 
 
@@ -56,7 +64,7 @@ def stack_data_and_metrics(data, metrics, metric_functions):
         data[metric_functions[i].__name__] = metric_data
     return data
 
-def transform_to_dict(data):
+def tranform_to_dict(data):
     # create a dict for easy use
     data_dict = {'open': data[:, 0],
                  'high': data[:, 1],
@@ -64,19 +72,6 @@ def transform_to_dict(data):
                  'close': data[:, 3],
                  'volume': data[:, 4],
                  'adjusted_close': data[:, 5]}
-
-    return data_dict
-
-def data_classes(data_dict):
-    "Determine classes of the data. Data is assumed to be in dictionary form."
-
-    # classes of the data
-    classes = cls.lr_classes(data_dict['adjusted_close'], period = 30, slope_quant = 2)
-    null_len = data_dict['adjusted_close'].shape[0] - classes.shape[0]
-    null_part = np.zeros((null_len, classes.shape[1]))
-    null_part.fill(np.nan)
-    for i in range(0, classes.shape[1]):
-        data_dict['zclasses' + str(i + 1)] = np.hstack((null_part[:, i], classes[:, i]))
     return data_dict
 
 def data_handler(which_stock, start_date, end_date, metric_functions, is_save_csv=True):
@@ -84,10 +79,7 @@ def data_handler(which_stock, start_date, end_date, metric_functions, is_save_cs
     stock = yahoo_finance_io.data_getter(which_stock, start_date, end_date).as_matrix()[:, :][::-1]
 
     # Tranform array to dict for easy use
-    stock = transform_to_dict(stock)
-
-    # Determine classes of the data
-    stock = data_classes(stock)
+    stock = tranform_to_dict(stock)
 
     # Adjust data according to adjusted close
     stock = adjusted_data(stock)
@@ -104,7 +96,7 @@ def data_handler(which_stock, start_date, end_date, metric_functions, is_save_cs
     df = pd.DataFrame(stock)
     
     if is_save_csv:
-        df.to_csv("./data/"+which_stock+".csv")
+        df.to_csv("data/"+which_stock+".csv")
         
     return df
 
@@ -120,17 +112,17 @@ def main():
     start_date = datetime.datetime(2000, 1, 3)
     end_date = datetime.datetime(2016, 12, 30)
     
-    stock_names = ['spy','gdx','xlf','jnug','eem','nugt','vxx','iwm','gdxj','uso','efa','uvxy','qqq','fxi','jdst','ewz','xlu','xle','ung',\
-                  'xiv','xop','vwo','xlp','hyg','jnk','xli','tlt','tza','xlv','rsx','ugaz','amlp','dust','vea','iemg','uco','xlk','iau','gld',\
-                   'kre','sds','iyr','xrt','slv','dgaz','ewj','xbi','oih','ezu','xlb','lqd','bkln','vnq','ijh','labd','xly','iefa','spxu','xme',\
-                   'ewt','dxj','eww','spxs','dia','fas','ivv','ijr','tna','inda','ewg','pff','vgk','svxy','agg','ewh','dbc','ewc','iwf','epi','kbe',\
-                   'sso','vixy','oil','labu','tbt','sqqq','itb','tqqq','ewu','bnd','ewa','vti','voo','fez','emb','iwd','uup','ewy','fxn','xlre']
+    stock_names = ['spy', 'msft', 'aapl']
 
-    gaugeCounter = 1
     for stock in stock_names:
-        print(gaugeCounter)
         df = data_handler(stock, start_date, end_date, metric_functions, is_save_csv=True)
-        gaugeCounter += 1
+        print(df.tail())
+
+
+
+    df = pd.read_csv("data/spy.csv")
+    print(df.head())
+    print(df.tail())
 
     #
     # # plt.plot(sma_15_data, color='r')
