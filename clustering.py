90012efcc79d2f5622899004a668b0a5ce13f51f
Ugur Gudelek <ugurgudelek@gmail.com>
2017-04-12 14:14:31 +0300

    no_plot added

--- clustering.py
+++ clustering.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def hierarchical_clustering(features_df):
+def hierarchical_clustering(features_df, no_plot=True):
 
     pearson_corr = features_df.corr(method='pearson')
     dist = 1 - np.array(pearson_corr)
@@ -12,5 +12,5 @@ def hierarchical_clustering(features_df):
 
     linkage = hierarchy.linkage(dist, method='average', metric='euclidean')
     plt.figure()
-    dn = hierarchy.dendrogram(linkage, labels=features_df.columns.values, no_plot=True)
+    dn = hierarchy.dendrogram(linkage, labels=features_df.columns.values, no_plot=no_plot)
     return dn['ivl']
