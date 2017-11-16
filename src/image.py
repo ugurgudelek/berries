84de64ae4f3b4e4cd14ea5e32b3fc9733e3a22a8
Ugur Gudelek <ugurgudelek@gmail.com>
2017-11-16 05:29:55 +0300

    new feed image class

--- src/image.py
+++ src/image.py
@@ -1,8 +1,31 @@
 """Image construction"""
 
-class Image:
-    def __init__(self):
-        pass
+from utils import Bucket,normalize_column_based
+import numpy as np
 
-    def feed(self, row):
-        pass
+
+class ImageEngine:
+    def __init__(self, split_period=28, normalize=True):
+        self.split_period = split_period
+        self.normalize = normalize
+        self.image_bucket = Bucket(size=self.split_period)
+
+        self.image_container = []
+
+    def feed(self, row, old_close=None):
+        """row should be a dict and should have 'date' and 'data' keys
+        row = dict('date','data')
+        """
+        data = row['data']
+        date = row['date']
+
+        self.image_container.append(data)
+
+        self.image_bucket.put(data=data)
+
+        if self.image_bucket.full():
+            chunk = self.image_bucket.get_all_bucket()
+            if self.normalize:
+                chunk = normalize_column_based(image=chunk)  # normalize image
+            chunk = np.array(chunk).flatten()
+            return chunk
