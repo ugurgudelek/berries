fae5a310bd79f823266257125a8a9c008aa17283
Uğur Güdelek <ugurgudelek@gmail.com>
2017-11-17 20:29:04 +0300

    image engine now holds stocks seperately

--- src/image.py
+++ src/image.py
@@ -2,30 +2,44 @@
 
 from utils import Bucket,normalize_column_based
 import numpy as np
+import pandas as pd
+from collections import defaultdict
 
 
 class ImageEngine:
-    def __init__(self, split_period=28, normalize=True):
+    def __init__(self, stock_names, split_period=28, normalize=True):
+        self.stock_names = stock_names
         self.split_period = split_period
         self.normalize = normalize
-        self.image_bucket = Bucket(size=self.split_period)
+        self.image_buckets = defaultdict(Bucket).fromkeys(self.stock_names)
+        for key, item in self.image_buckets.items():
+            self.image_buckets[key] = Bucket(size=split_period)
 
-        self.image_container = []
+        self.image_container = defaultdict(list).fromkeys(self.stock_names)
+        for key, item in self.image_container.items():
+            self.image_buckets[key] = []
 
-    def feed(self, row, old_close=None):
-        """row should be a dict and should have 'date' and 'data' keys
-        row = dict('date','data')
+    def feed(self, row):
+        """row should be a dict and should have 'stock_name','date' and 'data' keys
+        row = dict('date','data','stock_name')
         """
         data = row['data']
         date = row['date']
+        stock_name = row['stock_name']
 
-        self.image_container.append(data)
+        image_bucket = self.image_buckets[stock_name]
+        image_bucket.put(data=data)
 
-        self.image_bucket.put(data=data)
-
-        if self.image_bucket.full():
-            chunk = self.image_bucket.get_all_bucket()
+        if image_bucket.full():
+            image = pd.DataFrame(image_bucket.get_all_bucket())
             if self.normalize:
-                chunk = normalize_column_based(image=chunk)  # normalize image
-            chunk = np.array(chunk).flatten()
-            return chunk
+                # normalization for image.
+                image = (image - image.mean()) / image.std()
+                # chunk = normalize_column_based(image=chunk)  # normalize image
+
+            image = image.as_matrix().flatten()
+            self.image_container[stock_name].append(image)
+            return image
+
+class Image:
+    pass
