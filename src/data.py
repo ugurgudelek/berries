89e5844ba9b05a106fe89675cacf0a0c1d5e9958
Uğur Güdelek <ugurgudelek@gmail.com>
2017-11-17 20:28:18 +0300

    Data class init

---
+++ src/data.py
@@ -0,0 +1,9 @@
+class Data:
+    def __init__(self):
+        self.date = None
+        self.stock_name = None
+        self.day_data = None
+        self.metric_data = None
+        self.label = None
+        self.feature_data = None
+        self.image = None
