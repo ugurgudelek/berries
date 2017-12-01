e557ccbdc4987d3616679d58f12c7ff3bae80de1
Uğur Güdelek <ugurgudelek@gmail.com>
2017-12-01 17:16:22 +0300

    run number and save instance implemented

--- src/main.py
+++ src/main.py
@@ -8,11 +8,13 @@ import cnn
 import engine
 
 # done 1: download 1-day data
-# todo: semi-done 2: calculate metrics if available
+# done: semi-done 2: calculate metrics if available
 # todo 3: maybe cluster features
 # done 4: calculate labels
 # done 5: create images if available
-# todo 6: train & validation
+# done 6: train
+# todo 6.2:validation
+# done 6.3:save_all_engines
 # todo 7: test
 # todo 8: plot
 # todo 9: make it parallel
@@ -28,10 +30,10 @@ STOCK_NAMES = ['spy', 'xlf', 'xlu', 'xle',
                'xlp', 'xli', 'xlv', 'xlk', 'ewj',
                'xlb', 'xly', 'eww', 'dia', 'ewg',
                'ewh', 'ewc', 'ewa']
-# STOCK_NAMES = ['spy']
 MAKE_STATIONARY = True
 NORMALIZE_IMAGE = True
 APPLY_TANH = True
+RUN_NUMBER = 4
 
 financeIO = LocalIO()
 metric_engine = metric.MetricEngine(stock_names=STOCK_NAMES)
@@ -39,7 +41,7 @@ metric_engine.add_default_metrics()
 label_engine = label.LabelEngine(financeIO=financeIO, make_stationary=MAKE_STATIONARY, apply_tanh=APPLY_TANH)
 image_engine = image.ImageEngine(stock_names=STOCK_NAMES, split_period=28, normalize=NORMALIZE_IMAGE)
 params = {"input_w": 28, "input_h": 28, "num_classes": 1, "batch_size": 10, "epochs": 100}
-cnn_engine = cnn.CNNEngine(params=params, model_save_path='../model')
+cnn_engine = cnn.CNNEngine(params=params, model_save_path='../model', run_number=RUN_NUMBER)
 
 
 
@@ -51,7 +53,9 @@ main_engine = engine.Engine(financeIO=financeIO,
                             stock_names=STOCK_NAMES,
                             make_stationary=MAKE_STATIONARY,
                             apply_tanh=APPLY_TANH,
-                            verbose=True)
+                            verbose=True,
+                            instance_path='../instance',
+                            run_number=RUN_NUMBER)
 
 main_engine.run(start_date=START_DATE, end_date=END_DATE)
 
