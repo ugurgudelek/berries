1c1385e2a503ed31356e413f572cc0d8d74ca3e5
Uğur Güdelek <ugurgudelek@gmail.com>
2017-12-08 02:05:38 +0300

    now has to_series and dataholder

--- src/data.py
+++ src/data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 class Data:
     def __init__(self):
         self.date = None
@@ -7,3 +8,40 @@ class Data:
         self.label = None
         self.feature_data = None
         self.image = None
+
+        self.series = pd.Series()
+
+    def to_series(self):
+        self.series['date'] = self.date
+        self.series['stock_name'] = self.stock_name
+        self.series['open'] = self.day_data['open'].values[0]
+        self.series['high'] = self.day_data['high'].values[0]
+        self.series['low'] = self.day_data['low'].values[0]
+        self.series['close'] = self.day_data['close'].values[0]
+        self.series['volume'] = self.day_data['volume'].values[0]
+        for (index, values) in self.metric_data.iteritems():
+            if index == 'close':
+                self.series['close_stationary'] = values
+                self.series[index] = values
+        self.series['label_stationary'] = self.label
+
+        for i,value in enumerate(self.image):
+            self.series['px'+str(i)] = value
+
+        return self.series
+
+
+class DataHolder:
+    def __init__(self):
+        self.storage = []
+
+        self.dataframe = pd.DataFrame()
+
+    def append(self, data):
+        self.storage.append(data)
+
+    def to_dataframe(self):
+
+        for data in self.storage:
+            self.dataframe = self.dataframe.append(data.to_series(), ignore_index=True)
+        return self.dataframe
