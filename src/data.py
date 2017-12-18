bdfa419545564ae84c04677d5cc4a0c23c382ac1
Ugur Gudelek <ugurgudelek@gmail.com>
2017-12-18 04:55:40 +0300

    100x faster to_series implementation

--- src/data.py
+++ src/data.py
@@ -13,24 +13,17 @@ class Data:
         self.series = pd.Series()
 
     def to_series(self):
-        self.series['date'] = self.date
-        self.series['stock_name'] = self.stock_name
-        self.series['open'] = self.day_data['open'].values[0]
-        self.series['high'] = self.day_data['high'].values[0]
-        self.series['low'] = self.day_data['low'].values[0]
-        self.series['close'] = self.day_data['close'].values[0]
-        self.series['volume'] = self.day_data['volume'].values[0]
+        self.series = pd.Series()
+        self.series = self.series.append(self.day_data.iloc[0])
+        self.series['label'] = self.raw_label
+
+        # to fix conflict with close
+        self.metric_data.rename({'close': 'close_stationary', 'volume': 'volume_metric'}, inplace=True)
+
+        self.series = self.series.append(self.metric_data)
         self.series['label_stationary'] = self.label
-        self.series['raw_label'] = self.raw_label
-        for (index, values) in self.metric_data.iteritems():
-            if index == 'close':
-                self.series['close_stationary'] = values
-            else:
-                self.series[index] = values
 
-
-        for i,value in enumerate(self.image):
-            self.series['px'+str(i)] = value
+        self.series = self.series.append(pd.Series(self.image))
 
         return self.series
 
@@ -47,11 +40,16 @@ class DataHolder:
 
     def save(self, path):
         self.to_dataframe()
+        print("Saving to the {}".format(path))
         self.dataframe.to_csv(path)
 
-    def to_dataframe(self):
+    def reset_storage(self):
+        self.storage = []
 
-        for data in self.storage:
+    def to_dataframe(self):
+        print("Constructing dataframe ...")
+        for i,data in enumerate(self.storage):
+            print(i,len(self.storage))
             if self.dataframe.shape[0] == 0: # it is empty
                 self.dataframe = pd.DataFrame(columns=data.to_series().keys().values)
             self.dataframe = self.dataframe.append(data.to_series(), ignore_index=True)
