60c9aacec5c6e31ed218d691ad4d749eed13a134
Uğur Güdelek <ugurgudelek@gmail.com>
2018-03-06 20:00:42 +0300

    changed dir for fresh start

---
+++ src/model.py
@@ -0,0 +1,29 @@
+"""
+Ugur Gudelek
+model
+ugurgudelek
+06-Mar-18
+finance-cnn
+"""
+
+import torch
+from torch import nn
+class CNN(nn.Module):
+    """
+
+    """
+    def __init__(self):
+        super(CNN, self).__init__()
+        pass
+
+    def forward(self, *input):
+        pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
