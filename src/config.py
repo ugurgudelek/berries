de4c6738b00a4b1e35a18ed963c37bd0ff27b02a
Ugur Gudelek <ugurgudelek@gmail.com>
2018-04-08 18:33:54 +0300

    config is more useful now

--- src/config.py
+++ src/config.py
@@ -6,6 +6,8 @@ ugurgudelek
 finance-cnn
 """
 
+
+
 class Config:
     """
 
@@ -13,6 +15,22 @@ class Config:
 
     def __init__(self):
         self.stocks_dir = '../input/raw_data'
+        self.stock_names = ['spy']
+        self.label_after = 20
+
+        self.input_size = 28
+        self.seq_length = 28
+        self.num_layers = 1
+        self.out_size = 1
+
+        self.train_batch_size = 100
+        self.valid_batch_size = 100
+
+        self.train_shuffle = True
+        self.valid_shuffle = False
+
+        self.epoch_size = 20
+        self.storage_names = ['y_hat', 'loss', 'y']
 
 
 def main():
