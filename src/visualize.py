6a503b6e72efa9a32b60b6c76dd1900b74ec3251
Ugur Gudelek <ugurgudelek@gmail.com>
2018-04-19 17:43:00 +0300

    visualization should be improved

--- src/visualize.py
+++ src/visualize.py
@@ -1,11 +1,38 @@
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 class Visualizer:
     """
 
     """
 
     def __init__(self):
-        pass
+        self.container = dict()
+
+        #todo - generalize
+        self.container['tloss'] = np.array([])
+        self.container['vloss'] = np.array([])
+
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(1, 1, 1)
+        plt.show(block=False)
+
+
+
+    def append_data(self, name, data):
+        self.container[name] = np.append(self.container[name], data)
+
+    def visualize(self):
+        self.ax.clear()
+
+        for key, value in self.container.items():
+            plt.plot(value, label=key)
+
+        plt.legend()
+        plt.pause(0.01)
+        self.fig.canvas.draw()
 
 
 def display_errors(history):
