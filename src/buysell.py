423fe065127d21128d32b996fbf7248c63da933e
Ugur Gudelek <ugurgudelek@gmail.com>
2018-06-01 19:27:25 +0300

    buysell ipython now working

--- src/buysell.py
+++ src/buysell.py
@@ -98,34 +98,61 @@ class BuySell:
 
 if __name__ == "__main__":
     np.random.seed(42)
-    initial_capital = 10000
-    size= 100
-    fake_dataframe = pd.DataFrame({'price':np.random.randint(100,150, size=size),
-                                   'directive':np.random.choice(['buy','hold','sell'], size=size, replace=True),
-                                   'name':np.random.choice(['xlp','xlu','xlv','xly','dia','ewa','ewc','ewg','ewh','ewj','eww','spy','xlb','xle','xlf','xli','xlk'], size=size, replace=True)})
+    initial_capital = 100000
+    # size = 100
+    # fake_dataframe = pd.DataFrame({'price':np.random.randint(100,150, size=size),
+    #                                'directive':np.random.choice(['buy','hold','sell'], size=size, replace=True),
+    #                                'name':np.random.choice(['xlp','xlu','xlv','xly','dia','ewa','ewc','ewg','ewh','ewj','eww','spy','xlb','xle','xlf','xli','xlk'], size=size, replace=True)})
 
     dataframe = pd.read_csv('../experiment/finance_cnn3/result.csv')
     prices = pd.read_csv('../dataset/finance/stocks/stocks.csv')
 
     dataframe = pd.merge(dataframe, prices, on=['date', 'name']).drop(['open','high','low','close', 'volume'], axis=1)
 
-    dataframe = dataframe.rename(columns={'adjusted_close':'price'})
+    dataframe = dataframe.rename(columns={'adjusted_close': 'price'})
 
     def custom_argmax(row):
-        if row['sell'] == 1:
+        row = row[['psell','pbuy','phold']]
+
+        argmax_idx = np.argmax(row.values)
+
+
+        if argmax_idx == 0:
             return 'sell'
-        if row['buy'] == 1:
+        if argmax_idx == 1:
             return 'buy'
         return 'hold'
 
-    # dataframe['directive'] = dataframe.apply(custom_argmax, axis=1)
-    # dataframe = dataframe.drop_duplicates()
+    def custom_argmax2(row):
+        row = row[['psell','pbuy','phold']]
+
+        argmax_idx = np.argmax(row.values)
+
+
+        if argmax_idx == 0:
+            return pd.Series([1,0,0])
+        if argmax_idx == 1:
+            return pd.Series([0,1,0])
+        return pd.Series([0,0,1])
+
+
+    dataframe['directive'] = dataframe.apply(custom_argmax, axis=1)
+    dataframe[['pmapsell', 'pmapbuy', 'pmaphold']] = dataframe.apply(custom_argmax2, axis=1)
+
+    dataframe.loc[((dataframe['pmaphold'] == 1) & (dataframe['rhold'] == 1) |
+                   (dataframe['pmaphold'] == 0) & (dataframe['rhold'] == 0))].shape[0] /     dataframe.shape[0]
+
+    dataframe.loc[((dataframe['pmapsell'] == 1) & (dataframe['rsell'] == 1) |
+                   (dataframe['pmapsell'] == 0) & (dataframe['rsell'] == 0))].shape[0] /     dataframe.shape[0]
+
+    dataframe.loc[((dataframe['pmapbuy'] == 1) & (dataframe['rbuy'] == 1) |
+                   (dataframe['pmapbuy'] == 0) & (dataframe['rbuy'] == 0))].shape[0] /     dataframe.shape[0]
 
     stock_names = dataframe['name'].unique()
     result_dict = dict()
     for stock_name in stock_names:
 
-        search_df = fake_dataframe.loc[dataframe['name'] == stock_name]
+        search_df = dataframe.loc[dataframe['name'] == stock_name]
 
         buysell = BuySell(capital=initial_capital)
 
