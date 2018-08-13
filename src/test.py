6e1adc46a5e1c75ddb1fc15170d5d52326b1eaf0
Ugur Gudelek <ugurgudelek@gmail.com>
2018-08-13 05:51:14 +0300

    so much effort on ssh :-1:

---
+++ src/test.py
@@ -0,0 +1,12 @@
+
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+
+import matplotlib.pyplot as plt
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2*np.pi*t)
+plt.plot(t, s)
+
+plt.title('About as simple as it gets, folks')
+plt.show()
