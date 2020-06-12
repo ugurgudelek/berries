d88ba47cf9216751ff25b69eb47000e5352fc44f
ugurgudelek <ugurgudelek@gmail.com>
2020-06-12 05:10:55 +0300

    Merged some of the old code base

--- .need-to-check/.dependencies/ssh/gateway.bat
+++
@@ -1,3 +0,0 @@
-TIMEOUT 3
-start cmd /k  "ssh -f ugudelek@193.140.108.118 -L 1111:10.5.150.165:22 -N" shell=True
-echo 'll'
--- .need-to-check/input/energy/raw_data/pv_generation_data/Readme.md
+++
@@ -1,7 +0,0 @@
-Datum: Date
-
-von: from hh:mm
-
-bis: to hh:mm
-
-50Hertz, Amprion, TenneT, TransnetBW : company name 
