2cf2cbac5e8a2f4034da26d3bbd2f572098a16dd
ugurgudelek <ugurgudelek@gmail.com>
2020-06-12 04:53:19 +0300

    Automatic num of features calculation before fcnn layer

--- berries/model/cnn.py
+++ berries/model/cnn.py
@@ -1,30 +1,31 @@
 __author__ = "Ugur Gudelek"
 __email__ = "ugurgudelek@gmail.com"
 
+import torch
 from torch import nn
 
+import functools
+import operator
 
 class CNN(nn.Module):
     """Basic Pytorch CNN implementation"""
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, input_dim):
         nn.Module.__init__(self)
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=20, kernel_size=(3, 6), stride=(1,2)),
+            nn.Conv2d(in_channels=in_channels, out_channels=20, kernel_size=3, stride=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3, 6)),
+            nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(in_channels=20, out_channels=50, kernel_size=(3, 6), stride=(1,2)),
+            nn.Conv2d(in_channels=20, out_channels=50, kernel_size=3, stride=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3, 6)),
+            nn.MaxPool2d(kernel_size=2),
+        )
 
-            nn.Conv2d(in_channels=50, out_channels=10, kernel_size=(3, 6), stride=(1,2)),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3, 6)))
-
+        num_features_before_fcnn = functools.reduce(operator.mul, list(self.feature_extractor(torch.rand(1, *input_dim)).shape))
 
         self.classifier = nn.Sequential(
-            nn.Linear(in_features=120, out_features=100),
+            nn.Linear(in_features=num_features_before_fcnn, out_features=100),
             nn.Linear(in_features=100, out_features=out_channels),
         )
 
