aa43cb0f16d8cdbb4defa8c1558bb8645050a45b
Uğur Güdelek <ugurgudelek@gmail.com>
2020-06-30 03:42:33 +0300

    cnn for chatter only implemented

--- berries/model/cnn.py
+++ berries/model/cnn.py
@@ -36,3 +36,37 @@ class CNN(nn.Module):
         out = out.view(batch_size, -1)  # flatten the vector
         out = self.classifier(out)
         return out
+
+class ChatterCNN(nn.Module):
+    """Chatter Pytorch CNN implementation"""
+
+    def __init__(self, in_channels, out_channels, input_dim):
+        nn.Module.__init__(self)
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=20, kernel_size=(3, 6), stride=(1,2)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(3, 6)),
+
+            nn.Conv2d(in_channels=20, out_channels=50, kernel_size=(3, 6), stride=(1,2)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(3, 6)),
+
+            nn.Conv2d(in_channels=50, out_channels=10, kernel_size=(3, 6), stride=(1,2)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(3, 6)))
+
+        num_features_before_fcnn = functools.reduce(operator.mul, list(self.feature_extractor(torch.rand(1, *input_dim)).shape))
+        print(num_features_before_fcnn)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=num_features_before_fcnn, out_features=100),
+            nn.Linear(in_features=100, out_features=out_channels),
+        )
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        out = self.feature_extractor(x)
+        out = out.view(batch_size, -1)  # flatten the vector
+        out = self.classifier(out)
+        return out
