87ccb105d4296f203c0519484c5d71e0d4ee983a
Uğur Güdelek <ugurgudelek@gmail.com>
2020-09-17 05:30:52 +0300

    Add ae, vae and pca models

--- berries/model/__init__.py
+++ berries/model/__init__.py
@@ -2,3 +2,6 @@ from .cnn import CNN, ChatterCNN
 # from .encoder_decoder_rnn import RNNPredictor
 from .lstm import LSTM
 # from .lstm_tagger import LSTMTagger
+from .ae import AE
+from .vae import VAE
+from .pca import PCA
---
+++ berries/model/vae.py
@@ -0,0 +1,54 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class VAE(nn.Module):
+    def __init__(self, input_dim, latent_dim):
+        super(VAE, self).__init__()
+
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+
+        self.fc1 = nn.Linear(self.input_dim, 400)
+        self.fc21 = nn.Linear(400, self.latent_dim)
+        self.fc22 = nn.Linear(400, self.latent_dim)
+        self.fc3 = nn.Linear(self.latent_dim, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, self.input_dim))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+    # Reconstruction + KL divergence losses summed over all elements and batch
+
+    def loss_function(self, recon_x, x, mu, logvar):
+        BCE = F.binary_cross_entropy(
+            recon_x, x.view(-1, self.input_dim), reduction='sum')
+
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return BCE + KLD
+
+    def latent(self, x):
+        mu, logvar = self.encode(x.view(-1, self.input_dim))
+        z = self.reparameterize(mu, logvar)
+        return z
