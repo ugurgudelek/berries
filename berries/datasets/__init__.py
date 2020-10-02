85114951cd5d2363a401698a0a931eda2b37ffbf
Uğur Güdelek <ugurgudelek@gmail.com>
2020-10-03 01:01:54 +0300

    Change package init method to clear importing

--- berries/datasets/__init__.py
+++
@@ -1,3 +0,0 @@
-from .mnist import MNIST
-from .flights import Flights
-from .uea_ucr_dataset import BerriesUEAUCRDataset
--- berries/model/__init__.py
+++
@@ -1,8 +0,0 @@
-from .cnn import CNN, ChatterCNN
-# from .encoder_decoder_rnn import RNNPredictor
-from .lstm import LSTM
-# from .lstm_tagger import LSTMTagger
-from .ae import AE
-from .vae import VAE
-from .pca import PCA
-from .vrae import VRAE
